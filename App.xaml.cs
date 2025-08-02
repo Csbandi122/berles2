fb4672a8609ea01c288ff4144dad86e939031b01
Andras Cseke <csekebandi@gmail.com>
2025-08-02 14:38:10 +0200

    aa

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using ToolRental.Data;
 
 namespace berles2
 {
@@ -9,6 +11,18 @@ namespace berles2
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Adatbázis és tesztadatok inicializálása
+            using (var context = new ToolRentalDbContext(
+                new DbContextOptionsBuilder<ToolRentalDbContext>()
+                    .UseSqlite("Data Source=ToolRental.db")
+                    .Options))
+            {
+                SeedData.Initialize(context);
+            }
 
+            base.OnStartup(e);
+        }
+    }
 }
